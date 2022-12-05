35df80023859a14237197ca3c3abade426827d97
Chenzhou <chenzhou.liao@etu.unice.fr>
2022-12-05 10:34:27 +0100

    Add file output redirection

--- internals/groovy/GroovuinoML/src/main/groovy/groovuinoml/main/GroovuinoML.java
+++ internals/groovy/GroovuinoML/src/main/groovy/groovuinoml/main/GroovuinoML.java
@@ -3,6 +3,8 @@ package groovuinoml.main;
 import groovuinoml.dsl.GroovuinoMLDSL;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
 
 /**
  * This main takes one argument: the path to the Groovy script file to execute.
@@ -13,9 +15,13 @@ import java.io.File;
  * @author Thomas Moreau
  */
 public class GroovuinoML {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		GroovuinoMLDSL dsl = new GroovuinoMLDSL();
 		if(args.length > 0) {
+
+			File output = new File(args[0].substring(0, args[0].lastIndexOf('.')) + ".txt");
+			PrintStream stream = new PrintStream(output);
+			System.setOut(stream);
 			dsl.eval(new File(args[0]));
 		} else {
 			System.out.println("/!\\ Missing arg: Please specify the path to a Groovy script file to execute");
