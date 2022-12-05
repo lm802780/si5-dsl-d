807f0d7c243b9fbb31c9197e051a6a61b3178b1d
Chenzhou <chenzhou.liao@etu.unice.fr>
2022-12-05 15:26:22 +0100

    Fix scenario 4

--- internals/groovy/GroovuinoML/scripts/scenario4.groovy
+++ internals/groovy/GroovuinoML/scripts/scenario4.groovy
@@ -1,9 +1,13 @@
+//multi state alarm
+
 sensor "button" pin 9
 actuator "led" pin 12
 actuator "buzzer" pin 11
 
 state "firstStep" means buzzer becomes high
+state "secondStep"
 state "thirdStep" means led becomes high and buzzer becomes low
+state "fourthStep"
 state "fifthStep" means led becomes low
 state "off" means led becomes low and buzzer becomes low
 
