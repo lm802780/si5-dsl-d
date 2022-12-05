e77935afe0cc63dfb3ef3ad22a922721efddc7c8
Chenzhou <chenzhou.liao@etu.unice.fr>
2022-12-05 10:46:32 +0100

    make scenario more "sexy"

--- internals/groovy/GroovuinoML/scripts/scenario1.groovy
+++ internals/groovy/GroovuinoML/scripts/scenario1.groovy
@@ -7,9 +7,9 @@ actuator "buzzer" pin 11
 state "on" means led becomes high and buzzer becomes high
 state "off" means led becomes low and buzzer becomes low
 
-initial "off"
+initial off
 
-from "on" to "off" when "button" becomes "low"
+from on to off when button becomes low
 from off to on when button becomes high
 
-export "Switch!"
+export "verySimpleAlarm"
